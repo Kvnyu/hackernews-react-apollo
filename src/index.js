3a18fa0fda9bcc325394116787bad12ad2f5dd7e
KEVIN YU <kevinyu1333@gmail.com>
2021-01-06 17:37:14 +1100

    refactoring files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import {
+    ApolloProvider,
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache
+}   from '@apollo/client';
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+});
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +25,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
